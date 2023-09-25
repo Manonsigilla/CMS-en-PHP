7191cf5f2266a36135a9105504350e12e26e1a5c
Manon Sigaud <sigaudmanon@gmail.com>
2023-09-25 15:18:00 +0200

    first commit Création des pages Mise en place de la base de données et du connect

---
+++ admin/dashboard.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+    Cette page ne doit s'afficher que si l'admin est connecté
+
+    Cette page doit permettre : 
+        - De créer une nouvelle page
+        - De créer un nouvel article
+        - De gérer les comptes utilisateurs
+
+    Sur cette page vous devez également afficher :
+        - Les derniers articles (les 5 derniers)
+        - Les dernières pages (les 5 dernières)
+        - Les derniers utilisateurs (les 5 derniers)
+
+    Vous devez avoir la possibilité de :
+        - Afficher la liste complète des articles (c'est une page à part entière)
+        - Afficher la liste complète des pages (c'est une page à part entière)
+        - Afficher la liste complète des utilisateurs (c'est une page à part entière)
+*/
+?>
