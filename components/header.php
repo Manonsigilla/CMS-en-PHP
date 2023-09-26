43cf1a1f3634a7d1d2b4a4f2f7a25e8b495fc048
Manon Sigaud <sigaudmanon@gmail.com>
2023-09-26 10:05:33 +0200

    Ajout de la page inscription

--- components/header.php
+++ components/header.php
@@ -1,15 +1,111 @@
-// inclure la base de données pour le header
-<?php require_once('connect.php'); ?>
+<!-- // inclure la base de données pour le header -->
+<?php 
+require_once('admin/connect.php');
+// on vérifie si l'utilisateur est connecté
+if(isset($_SESSION['id_user'])){
+    // on démarre la session d'utilisateur connecté 
+    session_start();
+    // on écrit la requête qui permet de récupérer les données de l'utilisateur
+    $requete = "SELECT * FROM users WHERE id_user = :id";
+    // on prépare la requête
+    $requete = $db->prepare($requete);
+    // on exécute la requête avec les bonnes données
+    $requete->execute(array(
+        "id" => $_SESSION['id_user']
+    ));
+    // on récupère les données de la base de données
+    $resultat = $requete->fetch();
+    // on stocke les données dans des variables
+    $id = $resultat['id_user'];
+    $nom = $resultat['nom_user'];
+    $prenom = $resultat['prenom_user'];
+    $mail = $resultat['mail_user'];
+    $pseudo = $resultat['pseudo_user'];
+    $password = $resultat['password_user'];
+    $niveau = $resultat['niveau_user'];
+} 
+
+// si l'utilisateur n'est pas connecté alors l'utilisateur peut se connecter via le formulaire dans le header
+// on vérifie si l'username et le password sont bons et on crée une session pour l'utilisateur en tenant compte de son niveau d'utilisateur. Si l'utilisateur est un admin alors on le redirige vers la page admin.php et si l'utilisateur est un membre alors on le laisse sur la page index.php
+
+if(isset($_POST['submitConnection'])){
+    // on récupère les données dans des variables
+    $pseudo = $_POST['username'];
+    $password = $_POST['pass'];
+
+    // on vérifie que les champs ne sont pas vides
+    if(!empty($_POST['username']) && !empty($_POST['pass'])) {
+        require_once('admin/connect.php');
+        // On écrit la requête qui permet de récupérer les données de l'utilisateur
+        $requete = "SELECT pass_user as pass FROM users WHERE pseudo_user = :pseudo";
+        // On prépare la requête
+        $requete = $db->prepare($requete);
+        // On exécute la requête avec les bonnes données
+        $requete->execute(array(
+            "pseudo" => $pseudo
+        ));
+        // on récupère les données de la base de données
+        $resultat = $requete->fetch();
+        // si on obtient pas de résultat alors on affiche un message d'erreur
+        if(!$resultat) {
+            $errorMessage = "<p>Mauvais identifiant ou mot de passe !</p>";
+        }
+        // si c'est bon alors on peut vérifier le mot de passe
+        if($resultat){
+            if(!password_verify($password, $resultat['pass'])){
+                $errorMessage = "<p>Mauvais identifiant ou mot de passe !</p>";
+            }
+            if(password_verify($password, $resultat['pass'])){
+                // on crée une session pour l'utilisateur en tenant compte de son niveau d'utilisateur. Si l'utilisateur est un admin alors on le redirige vers la page admin.php et si l'utilisateur est un membre alors on le laisse sur la page index.php
+                $_SESSION['pseudo'] = $pseudo;
+                $_SESSION['pass'] = $password;
+                // si c'est l'admin alors on lui attribue un niveau de connexion
+                $niveauRequete = "SELECT niveau_user as niveau FROM users WHERE pseudo_user = :pseudo";
+                $niveauRequete = $db->prepare($niveauRequete);
+                $niveauRequete->execute(array(
+                    "pseudo" => $pseudo
+                ));
+                $resultatNiveau = $niveauRequete->fetch();
+                if($resultatNiveau && $resultatNiveau['niveau'] == 1){
+                    $_SESSION['admin'] = $resultatNiveau['niveau'];
+                    // si c'est l'admin alors on le redirige vers la page dashboard.php
+                    header('Location: admin/dashboard.php');
+                }
+                // si c'est un membre alors on le redirige vers la page index.php
+                header('Location: index.php');
+            }
+        }
+    }
+}
+?>
 
 <header>
-    <a href="index.php"><img src="uploads/logo.png" alt="logo"></a>
+    <a href="../creationCMS/index.php"><img src="uploads/logo.png" alt="logo"></a>
     <nav>
         <ul>
-            <li><a href="index.php">Home</a></li>
+            <li><a href="../creationCMS/index.php">Home</a></li>
             <?php if (isset($_SESSION['id_user'])) : ?>
-                <li><a href="logout.php">Logout</a></li>
+                <li>
+                    <details>
+                        <summary class="summary-login">Profil</summary>
+                        <ul>
+                            <li><a href="profil.php">Profil</a></li>
+                            <li><a href="deconnexion.php">Déconnexion</a></li>
+                        </ul>
+                    </details>
+                </li>
             <?php else : ?>
-                <li><a href="login.php">Login</a></li>
+                <li><details>
+                <summary class="summary-login">Login</summary>
+                <form action="profil.php" method="post" class="form-login">
+                    <input type="text" name="username" placeholder="pseudo">
+                    <input type="password" name="pass" placeholder="Mot de passe">
+                    <input type="submitConnection" name="submit" value="Se connecter">
+                </form>
+                <?php if (isset($errorMessage)) : ?>
+                    <p><?php echo $errorMessage; ?></p>
+                <?php endif; ?>
+                </details></li>
                 <li><a href="inscription.php">Inscription</a></li>
             <?php endif; ?>
             
--- inscription.php
+++ inscription.php
@@ -1,11 +1,99 @@
+<?php
+    $errorMessage = "";
+    $isEverythingOk = true;
+    
+    // On vérifie si les champs ne sont pas vides
+    if(isset($_POST['submit'])){
+        // on récupère les données dans des variables
+        $firstname = $_POST['firstname'];
+        $name = $_POST['name'];
+        $username = $_POST['username'];
+        $pass = $_POST['pass'];
+        $mail = $_POST['email'];
+        
+        // on vérifie que le mot de passe fasse au moins 8 caractères
+        if(strlen($pass) < 8){
+            $errorMessage = "<p>Le mot de passe doit faire au moins 8 caractères</p>";
+            $isEverythingOk = false;
+        }
+        // Les champs ne sont pas vides alors on peut vérifier si le mot de passe fait au moins 8 caractères et que l'adresse mail n'existe pas déjà dans la base de données
+        if(empty($mail) && empty($pass) && empty($name) && empty($firstname) && empty($username)){
+            $errorMessage = "<p>Les champs ne peuvent pas être vides</p>";
+            $isEverythingOk = false;
+        }
+        // on vérifie que l'email est valide
+        if(!filter_var($mail, FILTER_VALIDATE_EMAIL)){
+            $errorMessage = "<p>Adresse mail invalide</p>";
+            $isEverythingOk = false;
+        }
+        // on vérifie que l'adresse mail n'existe pas déjà dans la base de données
+        if($isEverythingOk){
+            session_start();
+            require_once('admin/connect.php');
+            $requete = "SELECT nom_user as nom, prenom_user as prenom, pseudo_user as pseudo, pass_user as pass, mail_user as mail FROM users WHERE mail_user = :mail";
+            $requete = $db->prepare($requete);
+            $requete->execute(array(
+                "mail" => $mail
+            ));
+            $result = $requete->fetch();
+            if($result){
+                $errorMessage = "<p>Quelque chose s'est mal passé, impossible de t'inscrire</p>";
+            }
+            // Si les champs ne sont pas vides et que le mot de passe fait au moins 8 caractères et que l'adresse mail n'existe pas déjà dans la base de données alors on peut insérer les données dans la base de données
+            if(!$result){
+                // On hash le mot de passe
+                $hash = password_hash($pass, PASSWORD_DEFAULT);
+                // On écrit la requête qui permet d'insérer les données dans la base de données
+                $requeteInsert = "INSERT INTO users(nom_user, prenom_user, mail_user, pseudo_user, pass_user) VALUES(:nom, :prenom, :mail, :pseudo, :pass)";
+                // On prépare la requête
+                $requeteInsert = $db->prepare($requeteInsert);
+                // On exécute la requête avec les bonnes données
+                $requeteInsert->execute(array(
+                    "nom" => $name,
+                    "prenom" => $firstname,
+                    "mail" => $mail,
+                    "pseudo" => $username,
+                    "pass" => $hash
+                ));
+                // on connecte l'utilisateur automatiquement et on le redirige vers la page profil.php
+                if ($requeteInsert->rowCount() > 0) {
+                    // Insertion réussie
+                    $_SESSION['pseudo'] = $username;
+                    $_SESSION['password'] = $hash;
+                    header('Location: profil.php');
+                } else {
+                    $errorMessage = "<p>Quelque chose s'est mal passé, impossible de t'inscrire</p>";
+                }
+            }
+            
+        }
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
     <title>Inscription</title>
 </head>
 <body>
-    
+    <?php
+        //insertion du header
+        include_once('components/header.php');
+    ?>
+    <h1>Formulaire d'inscription</h1>
+    <form class="form-inscription" action="" method="post">
+        <input type="text" name="name" id="name" placeholder="Nom">
+        <input type="text" name="firstname" id="firstname" placeholder="Prénom">
+        <input type="text" name="username" id="pseudo" placeholder="pseudo">
+        <input type="password" name="pass" id="password" placeholder="Mot de passe">
+        <input type="email" name="email" id="email" placeholder="Email">
+        <p class="errorMessage"><?= $errorMessage; ?></p>
+        <input type="submit" name="submit" value="S'inscrire">
+    </form>
 </body>
 </html>
